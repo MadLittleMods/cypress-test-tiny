c86b10ac5eaa2ba8236ebbbef11cf3aef6ec7539
Eric Eastwood <contact@ericeastwood.com>
2019-11-11 16:44:02 -0600

    Demo for layout shift causing click to miss target
    See https://github.com/cypress-io/cypress/issues/5662

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,9 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe("page", () => {
+  it("shows something", async function() {
+    cy.visit(`./test.html`);
+
+    cy.get(".click-me").click();
+
+    cy.get(".click-me").contains("click me [am-i-clicked? true]");
+  });
+});
